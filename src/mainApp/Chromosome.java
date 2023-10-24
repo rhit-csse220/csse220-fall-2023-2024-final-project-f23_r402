1b1a62d02260d78d275c7acfff0c6028a28cf48a
chenp5 <chenp5@137.112.130.191>
2023-10-23 21:10:15 -0400

    Chromosome and Gene classes

--- src/mainApp/Chromosome.java
+++ src/mainApp/Chromosome.java
@@ -1,11 +1,39 @@
 package mainApp;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Chromosome {
-	private String fileData;
+	public static final int NUM_OF_GENES = 100;
+	public static final int NUM_PER_ROW = 10;
 	
-	public void storeChromosomeData(String s) {
-		this.fileData=this.fileData.concat(s);
+	private String fileData;
+	private Gene[] genes = new Gene[NUM_OF_GENES];
+	private double fitnessScore;
+	
+	Random r = new Random();
+	
+	// constructor
+	public Chromosome() {
+		//TODO idk what to put
 	}
 	
+	// methods
+	public void storeChromosomeData(String s) {
+		this.fileData = this.fileData.concat(s);
+	}
 	
+	public void calcFuction() {
+		//TODO calc + store fitness score
+		this.fitnessScore = 0;
+	}
+	
+	public void addGene(Gene gene) {
+		for (int i = 0; i < NUM_PER_ROW; i++) {
+			for (int j = 0; j < NUM_PER_ROW; j++) {
+				int bit = r.nextInt(0,2);
+				genes[i] = new Gene((char)(bit+'0'), true, Gene.GENE_SIDE*j, Gene.GENE_SIDE*i);
+			}
+		}
+	}
 }
--- src/mainApp/Gene.java
+++ src/mainApp/Gene.java
@@ -1,8 +1,65 @@
 package mainApp;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 public class Gene {
+	public static final int GENE_SIDE = 5; // side length of gene square
+	public static final Color GENE_0_COLOR = Color.BLACK;
+	public static final Color GENE_1_COLOR = Color.GREEN;
+	
+	
 	private char bit;
 	private boolean changeable;
+	// x and y are top left corners of square
+	private int x;
+	private int y;
 	
+	// constructor
+	public Gene(char bit, boolean changeable, int x, int y) {
+		this.bit = bit;
+		this.changeable = changeable;
+		this.x = x;
+		this.y = y;
+	}
 	
+	// methods
+	public void drawOn(Graphics g) {
+		// Treat Graphics as a Graphics2D
+		Graphics2D g2 = (Graphics2D) g;
+		
+		if (this.bit == 0) {
+			g2.setColor(GENE_0_COLOR);
+		} else if (this.bit == 1) {
+			g2.setColor(GENE_1_COLOR);
+		}
+		
+		// or fill rect with x and y without translating?
+		g2.translate(x, y);
+		g2.fillRect(0, 0, GENE_SIDE, GENE_SIDE);
+		g2.translate(-x, -y);
+	}
+	
+	@Override
+	public String toString() {
+		return "Gene [x=" + x + ", y=" + y + ", bit=" + bit + ", changeable=" + changeable + "]";
+	}
+	
+	// used for when the bit is 0 or 1
+	public void changeBit() {
+		if (this.bit == 0) {
+			this.bit = 1;
+		} else if (this.bit == 1) {
+			this.bit = 0;
+		}
+	}
+	
+	public void setNonChangeable() {
+		this.changeable = false;
+	}
+	
+	public void setChangeable() {
+		this.changeable = true;
+	}
 }
