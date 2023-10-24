26683cdad64202e8ada4d2259059bf397a350451
kennym1 <kennym1@137.112.205.240>
2023-10-23 20:14:21 -0400

    Added load button functionality

--- src/mainApp/ChromosomeViewer.java
+++ src/mainApp/ChromosomeViewer.java
@@ -1,6 +1,12 @@
 package mainApp;
 
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.List;
 
 import javax.swing.*;
 
@@ -40,7 +46,32 @@ public class ChromosomeViewer {
 		JButton mutateButton = new JButton("Mutate");
 		JLabel mRate = new JLabel("M Rate:_/N");
 		JTextField mRateField = new JTextField("1", textFieldWidth);
+		
 		JButton loadButton = new JButton("Load");
+		loadButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser();
+				fileChooser.setCurrentDirectory(new File("C:\\Users\\"));
+				int response = fileChooser.showSaveDialog(null);
+				
+				if (response==JFileChooser.APPROVE_OPTION) {
+					File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+					System.out.println(file);
+					try {
+						List<String> lines = Files.readAllLines(file.toPath());
+						for (String s : lines) {
+							System.out.println(s);
+						}
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+				}
+			}
+		});
+		
 		JButton saveButton = new JButton("Save");
 		
 		panel.add(mutateButton, BorderLayout.CENTER);
@@ -48,6 +79,7 @@ public class ChromosomeViewer {
 		panel.add(mRateField, BorderLayout.CENTER);
 		panel.add(loadButton, BorderLayout.CENTER);
 		panel.add(saveButton, BorderLayout.CENTER);
+		
 		frame.pack();
 	} // driverMain
 	
