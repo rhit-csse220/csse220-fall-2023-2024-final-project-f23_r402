5cbd6d15f15d764b25bbc6b81ddb5b2a7fb0a756
kenny-mjk <148152445+kenny-mjk@users.noreply.github.com>
2023-10-29 17:33:13 -0400

    Create BestFitLine2D.java

---
+++ src/mainApp/BestFitLine2D.java
@@ -0,0 +1,48 @@
+package mainApp;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Line2D;
+import java.util.ArrayList;
+
+import javax.swing.JComponent;
+
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+
+public class BestFitLine2D {
+    private double bestFitness, avgFitness, lowFitness;
+
+    public double getBestFitness() {
+        return bestFitness;
+    }
+
+    public void setBestFitness(double bestFitness) {
+        this.bestFitness = bestFitness;
+    }
+
+    public double getAvgFitness() {
+        return avgFitness;
+    }
+
+    public void setAvgFitness(double avgFitness) {
+        this.avgFitness = avgFitness;
+    }
+
+    public double getLowFitness() {
+        return lowFitness;
+    }
+
+    public void setLowFitness(double lowFitness) {
+        this.lowFitness = lowFitness;
+    }
+
+    public BestFitLine2D(double bestFitness, double avgFitness, double lowFitness) {
+        this.bestFitness = bestFitness;
+        this.avgFitness = avgFitness;
+        this.lowFitness = lowFitness;
+    }
+}
