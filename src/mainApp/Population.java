ebe9fbcbf4eaf1f94095612af42a1742d6426f27
kenny-mjk <148152445+kenny-mjk@users.noreply.github.com>
2023-10-29 17:32:32 -0400

    Update Population.java

--- src/mainApp/Population.java
+++ src/mainApp/Population.java
@@ -4,21 +4,28 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Population {
-    private ArrayList<Chromosome> chromosomes = new ArrayList<Chromosome>();
+    public ArrayList<Chromosome> chromosomes = new ArrayList<Chromosome>();
     private int sizeOfPopulation = 100;
+    private int genomeLength = 100;
+    public double prevC,nextC, prevCLow, nextCLow;
+    public int prevCAvg,nextCAvg;
+    public ArrayList<BestFitLine2D> lineArray = new ArrayList<>();
 
     public Population(){
-        this.initiatePopulation();
     }
 
-    public Population(int sizeOfPopulation){
+    public Population(int sizeOfPopulation, int genomeLength){
         this.sizeOfPopulation = sizeOfPopulation;
+        this.genomeLength = genomeLength;
         this.initiatePopulation();
     }
 
     public void initiatePopulation(){
+        // chromosomes.removeAll(chromosomes);
+        chromosomes = new ArrayList<Chromosome>();
+        lineArray = new ArrayList<BestFitLine2D>();
         for (int i = 0; i<sizeOfPopulation; i++){
-            chromosomes.add(new Chromosome());
+            chromosomes.add(new Chromosome(genomeLength));
             chromosomes.get(i).initiateGene();
         }
         this.sortPopulation();
@@ -36,12 +43,36 @@ public class Population {
             this.chromosomes.remove(i);
             i-=1;
         }
-        for (int j = 0; j < initialSize/2; j++){
-            Chromosome newGenChromosome = new Chromosome(this.chromosomes.get(j).getChromosomeDataAsString());
-            newGenChromosome.mutateGenes(mutationRate);
-            this.chromosomes.add(newGenChromosome);
+        prevC = this.chromosomes.get(0).getFitnessScore();
+        prevCAvg = calculateAvgFitness();
+        prevCLow = this.chromosomes.get((initialSize/2)-1).getFitnessScore();
+        lineArray.add(new BestFitLine2D(prevC, prevCAvg, prevCLow));
+        ArrayList<Chromosome> cloneArray = new ArrayList<>();
+        for (Chromosome c : this.chromosomes){
+            cloneArray.add(new Chromosome(c.getChromosomeDataAsString()));
         }
-        this.giveFitness();
+        this.chromosomes = new ArrayList<Chromosome>();
+        for (int j = 0; j < initialSize/2; j++){
+            for (int copy2 = 0; copy2 < 2; copy2++){
+                Chromosome newGenChromosome = new Chromosome(cloneArray.get(j).getChromosomeDataAsString());
+                newGenChromosome.mutateGenes(mutationRate);
+                this.chromosomes.add(newGenChromosome);
+        }
+        }
+        this.sortPopulation();
+        // nextC = this.chromosomes.get(0);
+        // nextCAvg = calculateAvgFitness();
+        // nextCLow = this.chromosomes.get(this.chromosomes.size()-1);
+        //this.giveFitness();
+    }
+
+    public int calculateAvgFitness(){
+        int avg = 0;
+        for (Chromosome chromosome : chromosomes){
+            avg+= chromosome.getFitnessScore();
+        }
+        avg = avg/chromosomes.size();
+        return avg;
     }
 
     //Debugger to check if the sorting by fitness gave the correct result
