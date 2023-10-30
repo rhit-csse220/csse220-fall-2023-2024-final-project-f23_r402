5c15165378952a76a169c10302852958a123c254
Alex <anisima@rose-hulman.edu>
2023-10-29 20:51:25 -0400

    changed Chromosome.java to implement better practices. Genome Length in GUI crashes

--- src/mainApp/ChromosomeComponent.java
+++ src/mainApp/ChromosomeComponent.java
@@ -66,16 +66,16 @@ public class ChromosomeComponent extends JComponent{
 	 * ensures: to access the chromosome and store the data being read from the file into the chromosome's file data
 	 * @param s is the file data being passed back to be added to the chromosome's file data
 	 */
-    public void handleStoreChromosomeData(String s) {
-        this.chromosome.storeChromosomeData(s);
-    } //handleStoreChromosomeData
+    // public void handleStoreChromosomeData(String s) {
+    //     this.chromosome.storeChromosomeData(s);
+    // } //handleStoreChromosomeData
     
     /**
      * ensures: that the chromosome is able to load in its genes according to the values given in the chromosome's file data
      */
-    public void handleInitiateGeneWithFile() {
-        this.chromosome.initiateGeneWithFile();
-    } //handleInitiateGeneWithFile
+    // public void handleInitiateGeneWithFile() {
+    //     this.chromosome.initiateGeneWithFile();
+    // } //handleInitiateGeneWithFile
 	
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -91,18 +91,14 @@ public class ChromosomeComponent extends JComponent{
 	 */
 	public Gene containsGene(int x, int y) {
 		Rectangle2D.Double box = new Rectangle2D.Double(x-X_MOUSE_COORD_OFFSET, y-Y_MOUSE_COORD_OFFSET, 1, 1); // Mouse click coordinates were at an offset
-		for (int i = 0; i < chromosome.genes.length; i++) {
-			if (chromosome.genes[i].isSelected(box)) {
-				return chromosome.genes[i];
-			}
-		}
-		return null;
+		return chromosome.handleGetSelectedGene(box);
 	} //containsGene
 
 	public void handleLoadDataFromFile(String fileData) {
 		this.setChromosome(new Chromosome());
-		this.handleStoreChromosomeData(fileData);
-		this.handleInitiateGeneWithFile();
+		this.chromosome.initiateGeneWithString(fileData);
+		// this.handleStoreChromosomeData(fileData);
+		// this.handleInitiateGeneWithFile();
 	}
 
 	public void handleMutateGenesInChromosome(double mutationRate) {
