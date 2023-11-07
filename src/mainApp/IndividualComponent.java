c84299a842a41b1ba1a41f8a412885159e24eddd
kenny-mjk <kennym1@rose-hulman.edu>
2023-11-07 15:29:11 -0500

    removed magic numbers + fixed scaling using modified version of PJ's code

--- src/mainApp/IndividualComponent.java
+++ src/mainApp/IndividualComponent.java
@@ -5,21 +5,38 @@ import java.awt.Graphics2D;
 
 
 public class IndividualComponent extends Component{
-
+    
+    final static int DEFAULT_BORDER = 3;
+    final static int NUM_PER_ROW = 10;
+    final static int MULTIPLIER = 2;
+    final static int BEST_CHROMOSOME_INDEX = 0;
+    
     public IndividualComponent(){}
     private int index;
 
     @Override
     public void paint(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        int geneWidth = (int)(this.getWidth() / (this.population.getChromosomes().get(0).getNumPerColumn()) * .9);
+        int geneWidth = findGeneWidth();
         if (population != null && !population.getChromosomes().isEmpty()) {
             if (index >= 0 && index < population.getChromosomes().size()) {
                 population.getChromosomes().get(index).drawOn(g2, geneWidth, 0);
             } else {
                 // Handle the case when the index is out of bounds by displaying the first chromosome
-                population.getChromosomes().get(0).drawOn(g2, geneWidth, 0);
+                population.getChromosomes().get(BEST_CHROMOSOME_INDEX).drawOn(g2, geneWidth, 0);
             }
         }
     }
+
+    public int findGeneWidth() {
+		int compHeight = this.getHeight();
+		int compWidth = this.getWidth();
+
+        int numColumn = population.getChromosomes().get(0).getNumPerColumn();
+        if (numColumn < 10){
+            return (compWidth - DEFAULT_BORDER * MULTIPLIER) / (NUM_PER_ROW);
+        } else{
+            return (compHeight) / numColumn;
+        }
+	}
 }
