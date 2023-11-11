e4960263c412cf22d412a0d96fae66666734acf8
JasonHua000 <kuraken2004@gmail.com>
2023-11-11 00:50:50 -0500

    added javadoc EvolutionWorker

--- src/mainApp/EvolutionWorker.java
+++ src/mainApp/EvolutionWorker.java
@@ -6,6 +6,12 @@ import javax.swing.SwingUtilities;
 import javax.swing.SwingWorker;
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * The EvolutionWorker class extends SwingWorker and represents the background
+ * worker responsible for handling the evolution process in the EvolutionComponent.
+ * It operates perform evolutionary iterations and update the UI,
+ * and handle user interactions such as pausing and auto-stopping.
+ */
 public class EvolutionWorker extends SwingWorker<Void, Void> {
     private EvolutionComponent evComponent;
     private int generations;
@@ -13,27 +19,15 @@ public class EvolutionWorker extends SwingWorker<Void, Void> {
     private boolean shutAllFrames;
     private volatile boolean paused = false;
     private boolean autoStopEnabled = false;
-    private CountDownLatch pauseLatch = new CountDownLatch(1); // Add this line
+    private CountDownLatch pauseLatch = new CountDownLatch(1);
 
-
-    public void setPaused(boolean paused) {
-        this.paused = paused;
-        if (!paused) {
-            pauseLatch.countDown(); // Resume the worker
-        }
-    }
-    public void setAutoStopEnabled(boolean autoStopEnabled) {
-        this.autoStopEnabled = autoStopEnabled;
-    }
-    
-    public boolean isShutAllFrames() {
-        return shutAllFrames;
-    }
-
-    public void setShutAllFrames(boolean shutAllFrames) {
-        this.shutAllFrames = shutAllFrames;
-    }
-
+    /**
+     * Constructs an EvolutionWorker with the specified EvolutionComponent, number of generations,
+     * and the JButton used to start evolution.
+     * @param evComponent          The EvolutionComponent instance for evolution handling.
+     * @param generations          The total number of generations for evolution.
+     * @param startEvolutionButton The JButton used to start the evolution process.
+     */
     public EvolutionWorker(EvolutionComponent evComponent, int generations, JButton startEvolutionButton) {
         this.evComponent = evComponent;
         this.generations = generations;
@@ -41,33 +35,69 @@ public class EvolutionWorker extends SwingWorker<Void, Void> {
         this.shutAllFrames = false;
     }
 
+    /**
+     * Sets the paused state of the worker.
+     * @param paused True if the worker should be paused, false otherwise.
+     */
+    public void setPaused(boolean paused) {
+        this.paused = paused;
+        if (!paused) {
+            pauseLatch.countDown(); // Resume the worker
+        }
+    }
+
+    /**
+     * Sets the auto-stop feature of the worker.
+     * @param autoStopEnabled True if auto-stop is enabled, false otherwise.
+     */
+    public void setAutoStopEnabled(boolean autoStopEnabled) {
+        this.autoStopEnabled = autoStopEnabled;
+    }
+
+    /**
+     * Checks if all frames should be shut down.
+     * @return True if all frames should be shut down, false otherwise.
+     */
+    public boolean isShutAllFrames() {
+        return shutAllFrames;
+    }
+
+    /**
+     * Sets the flag to shut down all frames.
+     * @param shutAllFrames True if all frames should be shut down, false otherwise.
+     */
+    public void setShutAllFrames(boolean shutAllFrames) {
+        this.shutAllFrames = shutAllFrames;
+    }
+
+    /**
+     * The background task that performs evolutionary iterations and updates the UI.
+     * @return null when the task is complete.
+     * @throws Exception if an error occurs during the background task.
+     */
     @Override
     protected Void doInBackground() throws Exception {
         for (int generationCount = 0; generationCount <= generations; generationCount++) {
             evComponent.handleSelection();
             publish();
 
-            // Check if paused and wait
             while (paused) {
                 try {
-                    pauseLatch.await(); // Wait until signaled to resume
+                    pauseLatch.await(); 
                 } catch (InterruptedException e) {
                     Thread.currentThread().interrupt();
                 }
             }
 
-               if (autoStopEnabled && evComponent.checkForFitness100()) {
-                // Automatically reset the button to "Start Evolution" when fitness is 100
+            if (autoStopEnabled && evComponent.checkForFitness100()) {
                 final int generationCounts = generationCount;
                 SwingUtilities.invokeLater(() -> {
                     startEvolutionButton.setText("Start Evolution");
                     JOptionPane.showMessageDialog(null, "The first generation with perfect genes is " + generationCounts, "Perfect Genes Found", JOptionPane.INFORMATION_MESSAGE);
                 });
-                return null; // Exit the loop and stop the worker
+                return null;
             }
-            
 
-            // Repaint the graph (update the UI)
             SwingUtilities.invokeLater(() -> {
                 evComponent.repaint();
             });
@@ -80,8 +110,4 @@ public class EvolutionWorker extends SwingWorker<Void, Void> {
 
         return null;
     }
-    
 }
-
-
-
